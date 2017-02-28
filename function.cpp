c31bd487983b91e5142bb256fc48bd6d692a94b7
Andy Little <kwikius@yahoo.com>
2017-02-28 13:39:23 +0000

    start more generic functions

---
+++ function.cpp
@@ -0,0 +1,15 @@
+
+#include "function.hpp"
+#include "funtab.hpp"
+
+
+apm_mix::funtab_item_t* 
+apm_mix::funtab_t::find_item( const char* name)
+{
+   return nullptr;
+}
+
+void apm_mix::funtab_t::add_item(const char* name, apm_mix::funtab_item_t * node)
+{
+    
+}
--- function.hpp
+++ function.hpp
@@ -1,29 +1,31 @@
 #ifndef MIXER_LANG_FUNCTION_HPP_INCLUDED
 #define MIXER_LANG_FUNCTION_HPP_INCLUDED
 
+#include "exprtree.hpp"
+
 namespace apm_mix{
 
-   struct expr_list;
-
-   struct abc_function_t{
-      abc_function_t(int8_t num_args_in)
-      : m_num_args{num_args_in}
-      {}
-     private: m_num_args
-   }
-
-   template <int NumArgs>
-   struct function_t{
-      function_t: abc_function_t{NumArgs}{}
+   struct arg_list{
+      abc_expr * m_arg;
+      arg_list * m_next;
    };
 
-   struct function_call{
-       int8_t get_num_args();
-       
-        
+   struct function_builder {
+      function_builder(arg_list * args) : m_arg_list{args}{ }
+      virtual ~ function_builder()
+      { 
+         while ( m_arg_list != nullptr ){
+            auto* temp = m_arg_list;
+            m_arg_list = m_arg_list->m_next;
+            delete temp;
+         }
+      }
+      arg_list* m_arg_list;
+      // get_result_type
+      virtual bool type_check_args() const = 0;
+      
+      virtual abc_expr::exprID get_result_type() const = 0;
    };
-
 }
-
 
 #endif // MIXER_LANG_FUNCTION_HPP_INCLUDED
