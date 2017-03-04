cf9eae4689ec10a61ccf17bba1f3c2e8df4b812e
Andy Little <kwikius@yahoo.com>
2017-03-04 13:15:15 +0000

    document the interface in the main.cpp source file

--- examples/TaranisUSBJoystick/main.cpp
+++ examples/TaranisUSBJoystick/main.cpp
@@ -7,11 +7,24 @@
 
 namespace {
 
-   double airspeed = 0.0; // test pathological on airspeed based mixer
-   bool in_failsafe = false;  //
-   double get_airspeed(){ return airspeed;}
+   // This value simulates the airspeed sensor reading
+   // TODO make a way to vary it when running ... prob
+   // more suitable for a GUI version
+   double airspeed_m_per_s = 0.0; //
 
+   // true simulates a posible sensor failure 
+   // which may mean airspeed reading is no good
+   bool in_failsafe = false;  //
+
+   // The mixer uses functions pointers  to get its inputs
+   // The inputs can be of type Bool, Integer or Float 
+   double get_airspeed(){ return airspeed_m_per_s;}
+
+   // The input array represents all the available inputs
+   // which are passed to the mixer constructor
    // TODO make inputs optionally constant
+   // TODO add integer index inputs option
+
    apm_mix::input_pair inputs[]
     = { 
          apm_mix::input_pair{"Pitch", static_cast<double(*)()>(get_pitch)},
@@ -24,18 +37,44 @@ namespace {
          apm_mix::input_pair{"ARSPD_MIN", static_cast<double(*)()>([]()->double{return 10.0;})},
          apm_mix::input_pair{"ARSPD_CRUISE",static_cast<double(*)()>([]()->double{return 12.0;})},
          apm_mix::input_pair{"ARSPD_MAX", static_cast<double(*)()>([]()->double{return 20.0;})},
-         apm_mix::input_pair{"FAILSAFE_ON", static_cast<bool(*)()>([]()->bool{return in_failsafe;})}
+         // for completeness a bool input
+         apm_mix::input_pair{"FAILSAFE_ON", static_cast<bool(*)()>([]()->bool{return in_failsafe;})},
+         // for completeness an int input
+         apm_mix::input_pair{"DUMMY_INT", static_cast<int64_t(*)()>([]()->int64_t{return 1000;})}
       };
 
+   // The mixers also uses function pointers to send its outputs.
+   // outputs can also be of type Bool, Integer or Float
+   // Here for simplicity, we just pass an output function 'action'
+   // that prints the output to stdout
+   // The non type template parameter is handy to provide the index
    template<unsigned N>
    void action(double const & v)
    {
       printf("!!! output[%u] = %f\n",N,v);
    }
 
+   // to show that bool can be assigned as an ouput function also
+   template<unsigned N>
+   void action(bool const & v)
+   {
+      const char * const true_ = "true";
+      const char* const false_ = "false";
+      printf("!!! output[%u] = %s\n",N, (v? true_:false_));
+   }
+
+   
+   // Outputs can output a type of Integer, Bool or Float
+   // Allowing different output types may be overkill
+   // The types should probably be the same ( homogeneous)
+   // then you can switch physical rc output channels without issues.
+   // maybe though you want to set some channels true or false
+   // e.g leds indicators camera switches
+   // integer types can represent pwm exactly etc
+   // TBD
    apm_mix::abc_expr* outputs[]
    = {
-      new apm_mix::output<double>{action<0>}
+      new apm_mix::output<bool>{action<0>}
      , new apm_mix::output<double>{action<1>}
      , new apm_mix::output<double>{action<2>}
      , new apm_mix::output<double>{action<3>}
